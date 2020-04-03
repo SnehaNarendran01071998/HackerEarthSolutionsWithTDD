75630d42722db41f720ac428900e7a09b2dedb05
unknown <sneha.n21@gmail.com>
2020-04-03 12:03:39 +0530

    liftQueries|Add|Return nearest lift.|<Sneha Narendran>

--- src/liftQueries.java
+++ src/liftQueries.java
@@ -1,8 +1,25 @@
 public class liftQueries {
+    private int positionOfLiftA;
+    private int positionOfLiftB;
+
+    public liftQueries() {
+        this.positionOfLiftA = 0;
+        this.positionOfLiftB = 7;
+    }
+
     public char whichLift(int floorNumber) {
-        if (floorNumber == 0)
+        if (Math.abs(positionOfLiftA - floorNumber) <= Math.abs(positionOfLiftB - floorNumber)) {
             return 'A';
-        else
+        } else {
             return 'B';
+        }
+    }
+
+    public void moveLiftA(int floorNumber) {
+        positionOfLiftA = floorNumber;
+    }
+
+    public void moveLiftB(int floorNumber) {
+        positionOfLiftB = floorNumber;
     }
 }
--- tests/liftQueriesTest.java
+++ tests/liftQueriesTest.java
@@ -17,4 +17,27 @@ class liftQueriesTest {
         int floorNumber = 7;
         assertEquals('B', liftQueries.whichLift(floorNumber));
     }
+
+    @Test
+    void shouldReturnLiftAIfCalledFromFirstFloor() {
+        liftQueries liftQueries = new liftQueries();
+        int floorNumber = 3;
+        assertEquals('A', liftQueries.whichLift(floorNumber));
+    }
+
+    @Test
+    void shouldReturnLiftAIfCalledFromFifthFloorIfListAIsAtThirdFloor() {
+        liftQueries liftQueries = new liftQueries();
+        int floorNumber = 5;
+        liftQueries.moveLiftA(3);
+        assertEquals('A', liftQueries.whichLift(floorNumber));
+    }
+    @Test
+    void shouldReturnLiftBIfCalledFromThirdFloorIfListBIsAtFourthFloor() {
+        liftQueries liftQueries = new liftQueries();
+        int floorNumber = 3;
+        liftQueries.moveLiftB(4);
+        assertEquals('B', liftQueries.whichLift(floorNumber));
+    }
+
 }
